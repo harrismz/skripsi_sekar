6d3b0ec040546a0298c13b5a25f22d37c30b412a
sekarputri14 <116451074+sekarputri14@users.noreply.github.com>
2023-04-13 11:09:57 +0700

    automatic inser created by in model proses

--- app/Models/Proses.php
+++ app/Models/Proses.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 
 class Proses extends Model
 {
     protected $table = "proses";
-    use HasFactory;
+    use HasFactory,HasTimestamps;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function($model){
+            if(auth()->check()){
+                $model->dibuat_oleh = auth()->user()->nama;
+            }
+        });
+    }
 }
